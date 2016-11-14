d46c9a361c91cceeabaa59a5985a25b3651d1860
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-14 23:36:55 +0500

    * Refactoring: Variables renaming.

--- colors/colors.php
+++ colors/colors.php
@@ -8,7 +8,7 @@
  * @uses ManagerManager plugin 0.6.
  * 
  * @param $fields {comma separated string} — The name(s) of the template variables this should apply to. @required
- * @param $default {string} — Which color in hex format should be selected by default in new documents. This is only used in situations where the TV does not have a default value specified in the TV definition. Default: '#ffffff'.
+ * @param $defaultColor {string} — Which color in hex format should be selected by default in new documents. This is only used in situations where the TV does not have a default value specified in the TV definition. Default: '#ffffff'.
  * @param $roles {comma separated string} — The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
  * @param $templates {comma separated string} — Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
  * 
@@ -22,7 +22,7 @@
 
 function mm_widget_colors(
 	$fields,
-	$default = '#ffffff',
+	$defaultColor = '#ffffff',
 	$roles = '',
 	$templates = ''
 ){
@@ -45,9 +45,9 @@ function mm_widget_colors(
 		$fields = makeArray($fields);
 		
 		// Does this page's template use any of these TVs? If not, quit.
-		$tv_count = tplUseTvs($mm_current_page['template'], $fields);
+		$tvCount = tplUseTvs($mm_current_page['template'], $fields);
 		
-		if ($tv_count === false){return;}
+		if ($tvCount === false){return;}
 		
 		$output .= '//---------- mm_widget_colors :: Begin -----'.PHP_EOL;
 		
@@ -60,7 +60,7 @@ function mm_widget_colors(
 $j("#'.$tv_id.'").css("background-image","none");
 $j("#'.$tv_id.'").after(\'<div id="colorpicker'.$tv_id.'"></div>\').queue(function() {$j("#colorpicker'.$tv_id.'").farbtastic("#'.$tv_id.'");});
 if ($j("#'.$tv_id.'").val() == ""){
-	$j("#'.$tv_id.'").val("'.$default.'");
+	$j("#'.$tv_id.'").val("'.$defaultColor.'");
 }
 $j("#colorpicker'.$tv_id.'").mouseup(function(){
 	// mark the document as dirty, or the value wont be saved
